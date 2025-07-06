bed0146f639aba76c9e76589f5019744c1f12b65
amoljagtap17 <amoljagtap17@gmail.com>
2025-07-06 20:37:23 +0530

    add tanstack query setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,27 @@
 import { ThemeProvider } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
 import { theme } from "./app/theme";
 import { AppLayout } from "./components/layout";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
 
-      <AppLayout>
-        <App />
-      </AppLayout>
+      <QueryClientProvider client={queryClient}>
+        <AppLayout>
+          <App />
+        </AppLayout>
+
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>
 );
